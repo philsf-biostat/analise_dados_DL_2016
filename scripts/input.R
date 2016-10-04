0b583c2051e7d5d378caddefc8333649f2efb77e
Felipe Figueiredo <felipe.figueiredo@bio.fiocruz.br>
2016-10-04 13:41:51 -0300

    variáveis organizadas

--- scripts/input.R
+++ scripts/input.R
@@ -1,12 +1,11 @@
 dados <- read.csv2("dataset/dados_DL2016.csv")
 
+dados$A <- factor(dados$A)
+dados$B <- factor(dados$B)
 dados$OF <- factor(dados$OF)
 dados$OH <- factor(dados$OH)
 dados$IS <- ordered(dados$IS)
 dados$SUP <- factor(dados$SUP)
 dados$LADO <- factor(dados$LADO)
 dados$Falha <- factor(dados$Falha)
-dados$IS.2 <- ordered(dados$IS.2)
-dados$TOT <- ordered(dados$TOT)
-str(dados)
-
+dados$TOTAL <- ordered(dados$TOTAL)
