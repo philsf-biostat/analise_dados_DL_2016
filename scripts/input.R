458869b3c81450b90a68a8a4fc49b0f67a4918a9
Felipe Figueiredo <felipe.figueiredo@bio.fiocruz.br>
2016-10-04 13:45:05 -0300

    Fatores IS e TOTAL agrupados

--- scripts/input.R
+++ scripts/input.R
@@ -9,3 +9,7 @@ dados$SUP <- factor(dados$SUP)
 dados$LADO <- factor(dados$LADO)
 dados$Falha <- factor(dados$Falha)
 dados$TOTAL <- ordered(dados$TOTAL)
+
+# Agrupando fatores conforme requerido
+levels(dados$IS) <- list("<5" = 0:4, "5" = "5", ">5" = 6:11)
+levels(dados$TOTAL) <- list("<13" = 0:12, "13 a 14" = 13:14, "15 a 17" = 15:17, "18" = "18")
